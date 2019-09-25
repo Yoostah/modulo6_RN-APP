d3eba5cec52e9e207f96e911e186a458375dc8dd
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-24 22:39:23 -0300

    ajustes de funcionamento

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+// import {StyleSheet, View, Text} from 'react-native';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
