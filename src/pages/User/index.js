4775fa72260993da7ffa3774981db1df2c986caf
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-25 02:41:49 -0300

    app finalizado. Listando repositorios favoritados

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
-
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Repo,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -24,12 +34,38 @@ export default class User extends Component {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred/`);
+    const response = await api.get(`/users/${user.login}/starred`);
 
     this.setState({ starred: response.data });
   }
 
   render() {
-    return <View />;
+    const { starred } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={starred}
+          keyExtractor={repo => String(repo.id)}
+          renderItem={({ item }) => (
+            <Repo>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Repo>
+          )}
+        />
+      </Container>
+    );
   }
 }
